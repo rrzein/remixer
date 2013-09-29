afe014bd94dbaeaf1ad4000963a94daa6620e176
Ricky Zein <rrzein@gmail.com>
2013-09-28 20:09:30 -0700

    Initial commit

---
+++ js/remixer.js
@@ -0,0 +1,89 @@
+window.Remixer = (function() {
+
+  function Remixer(apiKey) {
+    this.apiKey = apiKey;
+    this.songs = new Array();
+    this.remixed = new Array();
+    this.init();
+  };
+
+// Initialize the environment (the Remixer and Player).
+
+  Remixer.prototype.init = function() {
+    if (window.webkitAudioContext == undefined) {
+      error("Your browser is wack. Upgrade to Chrome.");
+      return;
+    };
+    this.initRemixer();
+    this.initPlayer();
+  };
+
+  Remixer.prototype.initRemixer = function() {
+    var context = new webkitAudioContext();
+    this.remixer = createJRemixer(context, $, this.apiKey);
+    console.log("Remixer initialized.");
+  };
+
+  Remixer.prototype.initPlayer = function() {
+    this.player = this.remixer.getPlayer();
+    console.log("Player initialized.");
+  };
+
+// Add a new track to the remixer.
+
+  Remixer.prototype.addTrack = function(trackID, trackURL) {
+    var that = this;
+    this.remixer.remixTrackById(trackID, trackURL, function(t, percent) {
+      var track = t;
+
+      $("#info").text(percent + "% of the track loaded");
+  
+      if (percent == 100) {
+        $("#info").text(percent + "% of the track loaded, remixing...");
+      }
+
+      if (track.status == "ok") {
+        that.songs.push(track);
+        console.log("Track analyzed! Song Added.");
+        return;
+      }
+    })
+  };
+
+// Generate a remix from the songs.
+
+  Remixer.prototype.randomSong = function() {
+    var that = this;
+    var remixed = this.remixed;
+    var numberOfBeats = _.min(this.songs, function(song, index) {
+      return song.analysis.beats.length;
+    });
+
+    for (var i = 0; i < numberOfBeats; i++) {
+      if (i % 4 == 0 || i % 4 == 2) {
+        remixed.push(this.songs[0].analysis.beats[i]);
+      } else if (i % 4 == 1 || i % 4 == 3) {
+        remixed.push(this.songs[1].analysis.beats[i]);
+      }
+    }
+
+    return this.remixed;
+
+  };
+
+// Play the remixed song.
+
+  Remixer.prototype.play = function() {
+    this.player.stop();
+    this.player.play(0, this.remixed);
+  };
+
+// Pause the song currently playing.
+
+  Remixer.prototype.pause = function() {
+    this.player.stop();
+  };
+
+  return Remixer;
+
+})(this);
